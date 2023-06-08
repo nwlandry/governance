985f3480a727f93a7e4fecfe3c39a723bcb64a18
Nicholas Landry <nicholas.landry.91@gmail.com>
2023-06-08 09:13:52 -0400

    Update governance.py

--- governance/governance.py
+++ governance/governance.py
@@ -7,7 +7,7 @@ import xgi
 # optional parameters, which go in each group. Minimal number of parameters
 def decision_process(
     opinions,
-    decision_matrix,
+    decision_network,
     group_size,
     group_overlap,
     select_decision_type="random",
@@ -16,58 +16,58 @@ def decision_process(
     update_opinions_type="average",
 ):
 
-    num_decisions = np.size(decision_matrix, axis=0)
+    m = np.size(decision_network, axis=0)
 
     o = opinions.copy()
 
-    if np.size(decision_matrix, 0) != np.size(decision_matrix, 1):
+    if np.size(decision_network, 0) != np.size(decision_network, 1):
         raise Exception("Decision matrix must be square!")
 
-    if np.size(o, axis=1) != num_decisions:
+    if np.size(o, axis=1) != m:
         raise Exception("Opinion dimension doesn't match number of decisions")
 
-    num_policy_makers = np.size(o, axis=0)
-    policy_makers = list(range(num_policy_makers))
+    n = np.size(o, axis=0)
+    nodes = np.arange(n, dtype=int)
 
     completed_decisions = dict()
-    decisions = list(range(num_decisions))
+    decisions = list(range(m))
 
-    decision_groups = xgi.Hypergraph()
-    while len(completed_decisions) < num_decisions:
+    groups = xgi.Hypergraph()
+    while len(completed_decisions) < m:
         # Select the decision to make
         d = select_decision(
-            decisions, completed_decisions, decision_matrix, o, how=select_decision_type
+            decisions, completed_decisions, decision_network, o, how=select_decision_type
         )
 
         # Select the group to make that decision
         g = select_group(
-            policy_makers,
+            nodes,
             group_size,
             group_overlap,
             d,
-            decision_matrix,
+            decision_network,
             o,
-            decision_groups,
+            groups,
             how=select_group_type,
         )
 
-        decision_groups.add_edge(
+        groups.add_edge(
             g, id=d
         )  # add the group to the list of all decision groups
 
         # Make the decision
         completed_decisions[d] = make_decision(
-            d, g, decision_matrix, o, completed_decisions, how=make_decision_type
+            d, g, decision_network, o, completed_decisions, how=make_decision_type
         )
 
         # Update the group's opinions after the decision has been made.
-        o = update_opinions(o, g, decision_matrix, how=update_opinions_type)
+        o = update_opinions(o, g, decision_network, how=update_opinions_type)
 
-    return completed_decisions, o, decision_groups
+    return completed_decisions, o, groups
 
 
 def select_decision(
-    decisions, completed_decisions, decision_matrix, opinions, how="random"
+    decisions, completed_decisions, decision_network, opinions, how="random"
 ):
     """An algorithm for choosing which decision to make.
 
@@ -77,7 +77,7 @@ def select_decision(
         The list of all decisions.
     completed_decisions : list
         The list of decisions that have already been made.
-    decision_matrix : 2D numpy array
+    decision_network : 2D numpy array
         A symmetric matrix with -1, 0, and 1 elements.
         -1  indicates a contradictory relationship between decisions,
         0 indicates no relationship between decisions, and
@@ -116,7 +116,7 @@ def select_decision(
     # This randomly selects the decision proportional to the number of decisions
     # (positive or negative) to which it's connected.
     elif how == "degree":
-        d = np.sum(np.abs(decision_matrix), axis=0)
+        d = np.sum(np.abs(decision_network), axis=0)
         d = d[unmade_decisions] / np.sum(d[unmade_decisions])
         return np.random.choice(unmade_decisions, size=None, replace=True, p=d)
     else:
@@ -124,13 +124,13 @@ def select_decision(
 
 
 def select_group(
-    policy_makers,
+    nodes,
     size,
     overlap,
     decision,
-    decision_matrix,
+    decision_network,
     opinions,
-    H,
+    groups,
     how="random",
     **args
 ):
@@ -139,7 +139,7 @@ def select_group(
 
     Parameters
     ----------
-    policy_makers : list
+    nodes : list
         The list of all possible policy makers
     size: int
         group size
@@ -147,7 +147,7 @@ def select_group(
         number of bridge nodes
     decision : int
         The specific decision selected on which to decide.
-    decision_matrix : 2D numpy array
+    decision_network : 2D numpy array
         A symmetric matrix with -1, 0, and 1 elements.
         -1  indicates a contradictory relationship between decisions,
         0 indicates no relationship between decisions, and
@@ -157,7 +157,7 @@ def select_group(
         the column indicates the decision. The $ij$th entry is between -1 and 1.
         It indicates the opinion of decision-maker $i$ about decision $j$;
         -1 is fully against and 1 is fully for the decision.
-    H : xgi.Hypergraph
+    groups : xgi.Hypergraph
         A hypergraph tabulating all the past decision makers and decisions.
     how : str, default: "random"
         The method for choosing the policy makers for the selected decision. Options are
@@ -171,34 +171,41 @@ def select_group(
         the policy makers to decide on the selected decision.
     """
     n = np.size(opinions, axis=0)
-    policy_makers = set(H.nodes)
-    new_policy_makers = set(range(n)).difference(policy_makers)
+    nodes = set(groups.nodes)
+    new_nodes = set(range(n)).difference(nodes)
     if "random":
         # non-overlapping nodes: I had to take care of edge cases
         # (1) where there are no policy makers to begin with and
         # (2) where no policy makers have been absent from all decisions
-        new_nodes = min(size - overlap, len(new_policy_makers))
-        old_nodes = min(overlap, len(policy_makers))
-        g1 = random.sample(list(new_policy_makers), new_nodes)
-        g2 = random.sample(list(policy_makers), old_nodes)
+        new_nodes = min(size - overlap, len(new_nodes))
+        old_nodes = min(overlap, len(nodes))
+        g1 = random.sample(list(new_nodes), new_nodes)
+        g2 = random.sample(list(nodes), old_nodes)
         return set(g1).union(g2)
     else:
         raise Exception("Invalid group selection type!")
 
 
 def make_decision(
-    d, decision_group, decision_matrix, opinions, completed_decisions, how="average"
+    d, decision_group, decision_network, opinions, completed_decisions, how="average"
 ):
     # average opinions
     if how == "average":
         return np.mean(opinions[list(decision_group), d]) > 0
+    if how == "star":
+        config1 = -1
+        return config1
     else:
         raise Exception("Invalid decision making type!")
 
 
-def update_opinions(opinions, decision_group, decision_matrix, how="average"):
+def update_opinions(opinions, decision_group, d, decision_network, how="average"):
+    g = sorted(decision_group)
     if how == "average":
-        opinions[list(decision_group), :] = np.mean(opinions[list(decision_group), :])
+        opinions[g, :] = np.mean(opinions[g, :])
+    elif how == "sat":
+        
+        opinions[g, :] = np.mean(opinions[g, :])
     else:
         raise Exception("Invalid opinion update type!")
     return opinions
